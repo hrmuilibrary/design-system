4ff42df0f78df1adb709b9c105ba0a9859f9e0bf
manuktagushyan <manuk.tagushyan@ameriabank.am>
2026-08-10 12:22:19 +0400

    Add useFormContext hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,3 +4,4 @@ export { useOnOutsideClick } from './useOnOutsideClick';
 export { useEscapeKey } from './useEscapeKey';
 export { useHideBodyScroll } from './useHideBodyScroll';
 export { useIsMobile } from './useIsMobile';
+export { useFormContext } from './useFormContext';
